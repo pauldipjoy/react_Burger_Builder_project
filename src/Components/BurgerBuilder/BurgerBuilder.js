4e89b58e053353e1f314f8cd6249e839ca930769
DIPJOY PAUL ANTIK <antikpaul1994@gmail.com>
2022-04-05 00:33:51 +0600

    Initial Project setup

---
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class BurgerBuilder extends Component {
+
+    render(){
+        return(
+            <div>
+                <p>Burger Builder</p>
+            </div>
+        );
+    }
+
+
+
+}
---
+++ src/Components/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
+import Header from './Header/Header';
+
+
+
+const Main = props => {
+    return(
+        <div>
+
+            <Header />
+
+            <BurgerBuilder />
+
+        </div>
+    );
+}
+
+
+export default Main;
+
+
+
