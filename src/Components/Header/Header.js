200812405171787efea8a6ca22a02d0e08506cb9
DIPJOY PAUL ANTIK <antikpaul1994@gmail.com>
2022-04-05 19:52:11 +0600

    Creating Navigation Bar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { Nav, Navbar, NavbarBrand, NavItem, NavLink } from 'reactstrap';
+import Logo from '../../assets/logo.png';
+import './Header.css';
 
 const Header = () => {
+    
     return(
-        <div>
-            <p>Header</p>
+
+        <div className="Navigation">
+            <Navbar style={{
+                backgroundColor: "#d70f64",
+                height: "80px"
+
+            }} >
+                <NavbarBrand href="/" className="mr-auto ml-md-5 Brand">
+                    <img src={Logo} alt="Logo" width= "80px" />
+                </NavbarBrand>
+                <Nav className="mr-md-5">
+                    <NavItem>
+                        <NavLink href="#" className="NavLink">Something1</NavLink>
+                    </NavItem>
+                    
+                </Nav>
+            </Navbar>
+
+    
         </div>
     );
 }
-
 
 export default Header;
