136f19609fe64aa1a742d5b644a6bb1fcf6ea83c
DIPJOY PAUL ANTIK <antikpaul1994@gmail.com>
2022-04-09 13:51:16 +0600

    Add and Remove ingredients

--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -20,12 +20,45 @@ export default class BurgerBuilder extends Component {
     }
 
 
+    addIngredientHandle = type => {
+        // console.log(type);
+        const ingredients=[...this.state.ingredients];
+        //console.log(ingredients);
+        for(let item of ingredients){
+            if(item.type===type) item.amount++;
+        }
+        this.setState({ingredients: ingredients})
+
+    }
+
+    removeIngredientHandle = type => {
+        //console.log(type);
+        const ingredients=[...this.state.ingredients];
+        //console.log(ingredients);
+        for(let item of ingredients){
+            if(item.type===type) {
+
+                if(item.amount <= 0) return;
+                item.amount--;
+
+            };
+
+
+        }
+        this.setState({ingredients: ingredients})
+    }
+
+
 render(){
 
         return(
             <div className="d-flex flex-md-row flex-column">
                 <Burger ingredients={this.state.ingredients}/>
-                <Controls />
+                <Controls 
+                ingredientAdded={this.addIngredientHandle}
+                ingredientRemoved={this.removeIngredientHandle}
+                
+                />
             </div>
         );
     }
